16c857ffb57bf4b5f461c7d2946e234826b26bb4
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2024-04-25 16:50:44 +0200

    Beautify SQL queries.

--- validation_data/sql_request.sql
+++ validation_data/sql_request.sql
@@ -1,21 +1,64 @@
-select json_agg(foo) from(
-	(select images.id, collection_id, st_x(images.location) as lng, st_y(images.location) as lat , st_z(images.location) as alt, 
-	images.azimuth%360 as azimuth, images.tilt%360 as tilt, images.roll%360 as roll, 
-	images.focal, height, width, gcp_json, view_type
-	from images 
-	left join geolocalisations on images.geolocalisation_id = geolocalisations.id
-	where images.state = 'validated' and view_type like 'nadir'
-	order by random()
-	limit 100)
-) as foo
+SELECT
+  json_agg(foo)
+FROM
+  (
+    (
+      SELECT
+        images.id,
+        collection_id,
+        st_x(images.location) AS lng,
+        st_y(images.location) AS lat,
+        st_z(images.location) AS alt,
+        images.azimuth % 360 AS azimuth,
+        images.tilt % 360 AS tilt,
+        images.roll % 360 AS roll,
+        images.focal,
+        height,
+        width,
+        gcp_json,
+        view_type
+      FROM
+        images
+        LEFT JOIN geolocalisations ON images.geolocalisation_id = geolocalisations.id
+      WHERE
+        images.state = 'validated'
+        AND view_type LIKE 'nadir'
+      ORDER BY
+        random()
+      LIMIT
+        100
+    )
+  ) AS foo
 
-select json_agg(foo) from(
-	(select images.id, collection_id, st_x(images.location) as lng, st_y(images.location) as lat , st_z(images.location) as alt, 
-	images.azimuth%360 as azimuth, images.tilt%360 as tilt, images.roll%360 as roll, 
-	images.focal, height, width, gcp_json, view_type
-	from images 
-	left join geolocalisations on images.geolocalisation_id = geolocalisations.id
-	where images.state = 'validated' and view_type not in ('nadir', 'terrestrial')
-	order by random()
-	limit 100)
-) as foo
+
+SELECT
+  json_agg(foo)
+FROM
+  (
+    (
+      SELECT
+        images.id, 
+        collection_id, 
+        st_x(images.location) AS lng,
+        st_y(images.location) AS lat,
+        st_z(images.location) AS alt,
+        images.azimuth % 360 AS azimuth,
+        images.tilt % 360 AS tilt,
+        images.roll % 360 AS roll,
+        images.focal,
+        height,
+        width,
+        gcp_json,
+        view_type
+      FROM
+        images
+        LEFT JOIN geolocalisations ON images.geolocalisation_id = geolocalisations.id
+      WHERE
+        images.state = 'validated'
+        AND view_type NOT IN ('nadir', 'terrestrial')
+      ODER BY
+        random()
+      LIMIT
+        100
+    )
+  ) AS foo
